601e82d1aa97f7047d17d83e7be9611a644bf121
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2024-12-21 18:56:46 +0100

    config fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/full-stack-porfolio/', // Adjust this for GitHub Pages
+  base: '/my-website/', // Adjust this for GitHub Pages
   server: {
     port: 3000, // Change the dev server port if needed
   },
