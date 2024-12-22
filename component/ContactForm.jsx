2d626e5751f83b44283f5236494b362cdbe87877
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2024-12-22 13:20:42 +0100

    add nodemailer packages

--- component/ContactForm.jsx
+++ component/ContactForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
 import { Element } from 'react-scroll';
 
 
@@ -19,18 +18,23 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID, // Replace with your EmailJS Service ID
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // Replace with your EmailJS Template ID
-        {
-          from_name: formData.user_name,
-          from_email: formData.user_email,
-          message: formData.message,
-        },
-        import.meta.env.VITE_EMAILJS_USER_ID // Replace with your EmailJS User ID
-      );
+      try {
+        const reponse =
+         await fetch("http://localhost:5000/send", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        });
 
+        const result = await reponse.json();
+        if (reponse.ok){
+          setStatus("message sent successfully!");
+          setIsSubmitted(true);
+        }else {
+          setStatus(`Error: ${result.message}`);
+          setIsSubmitted(false);
+        }
+  
       // Reset form after successful submission
       setFormData({ user_name: '', user_email: '', message: '' });
       setIsSubmitted(true);
